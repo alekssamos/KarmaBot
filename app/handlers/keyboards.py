b0f3938800241be629869c1e7b8ae87d883d4f05
Yuriy Chebyshev <bomzheg@gmail.com>
2020-09-04 09:49:34 +0300

    first preparations

--- app/handlers/keyboards.py
+++ app/handlers/keyboards.py
@@ -1,12 +1,19 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.utils.callback_data import CallbackData
 
-from app.models import User, KarmaEvent
+from app.models import User, KarmaEvent, ModeratorEvent
 
 cb_karma_cancel = CallbackData("karma_cancel", "user_id", "action_id")
+cb_moderator_cancel = CallbackData("moderation_cancel", "user_id", "action_id")
 
 
 def get_kb_karma_cancel(user: User, action: KarmaEvent) -> InlineKeyboardMarkup:
     return InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(
         "Отменить", callback_data=cb_karma_cancel.new(user_id=user.tg_id, action_id=action.id_)
+    )]])
+
+
+def get_kb_moderation_cancel(user: User, action: ModeratorEvent) -> InlineKeyboardMarkup:
+    return InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(
+        "Отменить наказание", callback_data=cb_karma_cancel.new(user_id=user.tg_id, action_id=action.id_)
     )]])
--- app/services/moderation.py
+++ app/services/moderation.py
@@ -10,3 +10,13 @@ async def warn_user(moderator: User, target_user: User, chat: Chat, comment: str
         comment=comment
     )
 
+
+async def cancel_warn(moderator_event_id):
+    moderator_event = await ModeratorEvent.get(id_=moderator_event_id)
+    if moderator_event.type_restriction == "warn":
+        raise ValueError("asked for cancel warn but passed ", moderator_event.type_restriction)
+    await moderator_event.delete()
+
+
+async def ro_user(moderator: User, target_user: User, chat: Chat, comment: str):
+    pass
